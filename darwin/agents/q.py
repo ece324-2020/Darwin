04c66464dd5098881cd54061aaaf8590f9beb00d
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2020-11-27 08:30:53 -0500

    feat: update q learning algo to work

--- darwin/agents/q.py
+++ darwin/agents/q.py
@@ -13,36 +13,56 @@ EPSILON = 0.1
 class QAgent:
     def __init__(self, env):
         self.Q = collections.defaultdict(float)
-        self.actions = range(len(env.action_space.spaces.items()))
+        # Actions are 1x3 matrix, 12 discrete possible values for each element
+        # Change so each policy has its own index that is associated with a potential action/observation space
+        # Index created whenever policies are created
+        # This way if agents have different action spaces we are only choosing from their own
+        # I don't want to index it using variable from the MultiDiscrete class like this but don't know
+        # what choice we have
+        r1, r2, r3 = env.action_space.spaces['action_movement'].spaces[0].nvec
+        self.actions = [(x, y, z) for x in range(r1) for y in range(r2) for z in range(r3)]
         self.env = env
 
-    def update(self, s, a, s_next, done):
+    def update(self, s, r, a, s_n, done):
         """
         Args:
         - s: current observation
         - a: current action
         - s_next: next observation
         """
-        max_q_n = max([self.Q[s_n, a_n] for a_n in actions])
+        # print('s_n', s_n)
+        s_n_key = tuple(s_n['observation_self'][0])
+        max_q_n = max([self.Q[s_n_key, a_n] for a_n in self.actions])
         # Don't include next state if done is 1 (if at final state)
-        self.Q[s, a] += ALPHA * (r + GAMMA * max_q_n * (1 - done) - self.Q[s, a])
+        s_key = tuple(s['observation_self'][0])
+        a_key = tuple(a['action_movement'][0])
+        self.Q[s_key, a_key] += ALPHA * (r + GAMMA * max_q_n * (1 - done) - self.Q[s_key, a_key])
 
     def act(self, obs):
-        return self.env.action_space.sample()
         # # Force observation using epsilon-greedy
-        # if np.random.random() < EPSILON:
-        #     return self.env.action_space.sample()
-        
-        # # print('obs', obs)
-        # # print('0-----------------')
-        # # for a in self.actions:
-        # #     print(a)
+        if np.random.random() < EPSILON:
+            sample = self.env.action_space.sample()
+            new_sample = collections.OrderedDict()
+            new_sample['action_movement'] = [sample['action_movement'][0]]
+            print("-------------------------Random sample selected---------------------------")
+            return map_sample_to_action(sample, is_gym_space=True)
 
-        # action_val_pairs = [(a, self.Q[obs, a]) for a in self.actions]
-        # max_q = max(action_val_pairs, key=lambda x: x[1])
-        # action_choices = [a for a, q in action_val_pairs if q == max_q]
-        # # Select random action of action set with maximum reward value
-        # return np.random.choice(action_choices)
+        obs_key = tuple(obs['observation_self'][0])
+        action_val_pairs = [(a, self.Q[obs_key, a]) for a in self.actions]
+        max_q = max(action_val_pairs, key=lambda x: x[1])[1]
+        action_choices = [a for a, q in action_val_pairs if q == max_q]
+        # Select random action of action set with maximum reward value
+        choice = list(action_choices[np.random.choice(len(action_choices))])
+        return map_sample_to_action(choice)
 
     def reset(self):
         pass
+
+
+def map_sample_to_action(sample, is_gym_space=False):
+    new_sample = collections.OrderedDict()
+    if is_gym_space:
+        new_sample['action_movement'] = [sample['action_movement'][0]]
+    else:
+        new_sample['action_movement'] = [np.array(sample)]
+    return new_sample
--- darwin/viewer/train_viewer.py
+++ darwin/viewer/train_viewer.py
@@ -8,7 +8,7 @@ from mujoco_py import const, MjViewer
 from utils.util import listdict2dictnp
 
 
-STEPS = 1000
+STEPS = 300
 EPISODES = 10
 
 
@@ -23,149 +23,26 @@ def splitobs(obs, keepdims=True):
 
 
 class TrainViewer(MjViewer):
-    # def __init__(self, env, policies, display_window=True, seed=None, duration=None, episodes=EPISODES, steps=STEPS):
-    #     if seed is None:
-    #         self.seed = env.seed()[0]
-    #     else:
-    #         self.seed = seed
-    #         env.seed(seed)
-        
-    #     self.env = env
-    #     self.policies = policies
-    #     self.display_window = display_window
-    #     self.seed = seed
-    #     self.duration = duration
-    #     self.steps = steps
-    #     self.episodes = episodes
-
-    #     self.total_rew = 0.
-    #     self.ob = env.reset()
-
-    #     for policy in self.policies:
-    #         policy.reset()
-        
-    #     assert env.metadata['n_agents'] % len(policies) == 0
-    #     if hasattr(env, "reset_goal"):
-    #         self.goal = env.reset_goal()
-    #     super().__init__(self.env.unwrapped.sim)
-    #     # TO DO: remove circular dependency on viewer object. It looks fishy.
-    #     self.env.unwrapped.viewer = self
-    #     if self.render and self.display_window:
-    #         self.env.render()
-
-    # def key_callback(self, window, key, scancode, action, mods):
-    #     super().key_callback(window, key, scancode, action, mods)
-    #     # Trigger on keyup only:
-    #     if action != glfw.RELEASE:
-    #         return
-    #     # Increment experiment seed
-    #     if key == glfw.KEY_N:
-    #         self.reset_increment()
-    #     # Decrement experiment trial
-    #     elif key == glfw.KEY_P:
-    #         print("Pressed P")
-    #         self.seed = max(self.seed - 1, 0)
-    #         self.env.seed(self.seed)
-    #         self.ob = self.env.reset()
-    #         for policy in self.policies:
-    #             policy.reset()
-    #         if hasattr(self.env, "reset_goal"):
-    #             self.goal = self.env.reset_goal()
-    #         self.update_sim(self.env.unwrapped.sim)
-
-
-    # def run(self):
-    #     self.total_rew_avg = 0.0
-    #     self.n_episodes = 0
-    #     self.rewards = []
-    #     for episode in range(self.episodes):
-
-    #         for step in range(self.steps):
-    #             if len(self.policies) == 1:
-    #                 action, _ = self.policies[0].act(self.ob)
-    #                 last_act = action
-    #                 last_ob = self.ob
-    #             else:
-    #                 self.ob = splitobs(self.ob, keepdims=False)
-    #                 ob_policy_idx = np.split(np.arange(len(self.ob)), len(self.policies))
-    #                 last_ob = self.ob
-    #                 last_ob_idx = ob_policy_idx
-
-    #                 actions = []
-    #                 for i, policy in enumerate(self.policies):
-    #                     inp = itemgetter(*ob_policy_idx[i])(self.ob)
-    #                     inp = listdict2dictnp([inp] if ob_policy_idx[i].shape[0] == 1 else inp)
-    #                     ac = policy.act(inp)
-    #                     actions.append(ac)
-    #                 action = listdict2dictnp(actions, keepdims=True)
-
-    #             self.ob, rew, done, env_info = self.env.step(action)
-
-    #             # Update policies
-    #             # if len(self.policies) == 1:
-    #             #     policy.update(last_ob, last_act, self.ob, done)
-    #             # else:                    
-    #             #     ob = splitobs(self.ob, keepdims=False)
-    #             #     ob_policy_idx = np.split(np.arange(len(ob)), len(self.policies))
-
-    #             #     for i, a, policy in enumerate(zip(actions, self.policies)):
-    #             #         last_inp = itemgetter(*last_ob_idx[i])(last_ob)
-    #             #         last_inp = listdict2dictnp([last_inp] if last_ob_idx[i].shape[0] == 1 else last_inp)
-    #             #         inp = itemgetter(*ob_policy_idx[i])(ob)
-    #             #         inp = listdict2dictnp([inp] if ob_policy_idx[i].shape[0] == 1 else inp)
-    #             #         policy.update(last_inp, a, inp, done)
-
-
-    #             self.total_rew += rew
-
-    #             if done or env_info.get('discard_episode', False):
-    #                 self.reset_increment()
-
-    #             if self.display_window:
-    #                 self.add_overlay(const.GRID_TOPRIGHT, "Reset env; (current seed: {})".format(self.seed), "N - next / P - previous ")
-    #                 self.add_overlay(const.GRID_TOPRIGHT, "Reward", str(self.total_rew))
-    #                 if hasattr(self.env.unwrapped, "viewer_stats"):
-    #                     for k, v in self.env.unwrapped.viewer_stats.items():
-    #                         self.add_overlay(const.GRID_TOPRIGHT, k, str(v))
-
-    #                 self.env.render()
-
-    #         self.rewards.append(self.total_rew)
-
-    #     self.env.close()
-
-
-    # def reset_increment(self):
-    #     self.total_rew_avg = (self.n_episodes * self.total_rew_avg + self.total_rew) / (self.n_episodes + 1)
-    #     self.n_episodes += 1
-    #     print(f"Reward: {self.total_rew} (rolling average: {self.total_rew_avg})")
-    #     self.total_rew = 0.0
-    #     self.seed += 1
-    #     self.env.seed(self.seed)
-    #     self.ob = self.env.reset()
-    #     for policy in self.policies:
-    #         policy.reset()
-    #     if hasattr(self.env, "reset_goal"):
-    #         self.goal = self.env.reset_goal()
-    #     self.update_sim(self.env.unwrapped.sim)
-
-    def __init__(self, env, policies, display_window=True, seed=None, duration=None):
+    def __init__(self, env, policies, display_window=True, seed=None, duration=None, episodes=EPISODES, steps=STEPS):
         if seed is None:
             self.seed = env.seed()[0]
         else:
             self.seed = seed
             env.seed(seed)
-
+        
         self.env = env
         self.policies = policies
         self.display_window = display_window
-        # self.seed = seed
         self.duration = duration
+        self.steps = steps
+        self.episodes = episodes
 
-        self.total_rew = 0.0
+        self.total_rew = 0.
         self.ob = env.reset()
+
         for policy in self.policies:
             policy.reset()
+        
         assert env.metadata['n_agents'] % len(policies) == 0
         if hasattr(env, "reset_goal"):
             self.goal = env.reset_goal()
@@ -195,41 +72,70 @@ class TrainViewer(MjViewer):
                 self.goal = self.env.reset_goal()
             self.update_sim(self.env.unwrapped.sim)
 
+
     def run(self):
-        if self.duration is not None:
-            self.end_time = time.time() + self.duration
         self.total_rew_avg = 0.0
         self.n_episodes = 0
-        while self.duration is None or time.time() < self.end_time:
-            if len(self.policies) == 1:
-                action, _ = self.policies[0].act(self.ob)
-            else:
-                # self.ob = splitobs(self.ob, keepdims=False)
-                # ob_policy_idx = np.split(np.arange(len(self.ob)), len(self.policies))
-                # actions = []
-                # for i, policy in enumerate(self.policies):
-                #     inp = itemgetter(*ob_policy_idx[i])(self.ob)
-                #     inp = listdict2dictnp([inp] if ob_policy_idx[i].shape[0] == 1 else inp)
-                #     ac = policy.act(inp)
-                #     actions.append(ac)
-                # action = listdict2dictnp(actions, keepdims=True)
-                action = self.policies[0].act(self.ob)
-                action = listdict2dictnp([action], keepdims=True)
+        self.rewards = []
+        for episode in range(self.episodes):
+            print('#######################')
+            print('Episode # {}'.format(episode))
+            print('#######################')
+            for step in range(self.steps):
+                if len(self.policies) == 1:
+                    action, _ = self.policies[0].act(self.ob)
+                    last_act = action
+                    last_ob = self.ob
+                else:
+                    self.ob = splitobs(self.ob, keepdims=False)
+                    ob_policy_idx = np.split(np.arange(len(self.ob)), len(self.policies))
+                    last_ob = self.ob
+                    last_ob_idx = ob_policy_idx
+                    actions = []
+                    for i, policy in enumerate(self.policies):
+                        inp = itemgetter(*ob_policy_idx[i])(self.ob)
+                        inp = listdict2dictnp([inp] if ob_policy_idx[i].shape[0] == 1 else inp)
+                        ac = policy.act(inp)
+                        actions.append(ac)
+                    action = listdict2dictnp(actions, keepdims=True)
 
-            self.ob, rew, done, env_info = self.env.step(action)
-            self.total_rew += rew
+                self.ob, rew, done, env_info = self.env.step(action)
 
-            if done or env_info.get('discard_episode', False):
-                self.reset_increment()
+                print('IS ITER DONE', done)
 
-            if self.display_window:
-                self.add_overlay(const.GRID_TOPRIGHT, "Reset env; (current seed: {})".format(self.seed), "N - next / P - previous ")
-                self.add_overlay(const.GRID_TOPRIGHT, "Reward", str(self.total_rew))
-                if hasattr(self.env.unwrapped, "viewer_stats"):
-                    for k, v in self.env.unwrapped.viewer_stats.items():
-                        self.add_overlay(const.GRID_TOPRIGHT, k, str(v))
+                # Update policies
+                if len(self.policies) == 1:
+                    policy.update(last_ob, last_act, self.ob, done)
+                else:                    
+                    ob = splitobs(self.ob, keepdims=False)
+                    ob_policy_idx = np.split(np.arange(len(ob)), len(self.policies))
 
-                self.env.render()
+                    for i, (a, r, policy) in enumerate(zip(actions, rew, self.policies)):
+                        last_inp = itemgetter(*last_ob_idx[i])(last_ob)
+                        last_inp = listdict2dictnp([last_inp] if last_ob_idx[i].shape[0] == 1 else last_inp)
+                        inp = itemgetter(*ob_policy_idx[i])(ob)
+                        inp = listdict2dictnp([inp] if ob_policy_idx[i].shape[0] == 1 else inp)
+                        policy.update(last_inp, r, a, inp, done)
+
+
+                self.total_rew += rew
+
+                if done or env_info.get('discard_episode', False):
+                    self.reset_increment()
+
+                if self.display_window:
+                    self.add_overlay(const.GRID_TOPRIGHT, "Reset env; (current seed: {})".format(self.seed), "N - next / P - previous ")
+                    self.add_overlay(const.GRID_TOPRIGHT, "Reward", str(self.total_rew))
+                    if hasattr(self.env.unwrapped, "viewer_stats"):
+                        for k, v in self.env.unwrapped.viewer_stats.items():
+                            self.add_overlay(const.GRID_TOPRIGHT, k, str(v))
+
+                    self.env.render()
+
+            self.rewards.append(self.total_rew)
+
+        self.env.close()
+
 
     def reset_increment(self):
         self.total_rew_avg = (self.n_episodes * self.total_rew_avg + self.total_rew) / (self.n_episodes + 1)
@@ -244,3 +150,99 @@ class TrainViewer(MjViewer):
         if hasattr(self.env, "reset_goal"):
             self.goal = self.env.reset_goal()
         self.update_sim(self.env.unwrapped.sim)
+
+    # def __init__(self, env, policies, display_window=True, seed=None, duration=None):
+    #     if seed is None:
+    #         self.seed = env.seed()[0]
+    #     else:
+    #         self.seed = seed
+    #         env.seed(seed)
+
+    #     self.env = env
+    #     self.policies = policies
+    #     self.display_window = display_window
+    #     # self.seed = seed
+    #     self.duration = duration
+
+    #     self.total_rew = 0.0
+    #     self.ob = env.reset()
+    #     for policy in self.policies:
+    #         policy.reset()
+    #     assert env.metadata['n_agents'] % len(policies) == 0
+    #     if hasattr(env, "reset_goal"):
+    #         self.goal = env.reset_goal()
+    #     super().__init__(self.env.unwrapped.sim)
+    #     # TO DO: remove circular dependency on viewer object. It looks fishy.
+    #     self.env.unwrapped.viewer = self
+    #     if self.render and self.display_window:
+    #         self.env.render()
+
+    # def key_callback(self, window, key, scancode, action, mods):
+    #     super().key_callback(window, key, scancode, action, mods)
+    #     # Trigger on keyup only:
+    #     if action != glfw.RELEASE:
+    #         return
+    #     # Increment experiment seed
+    #     if key == glfw.KEY_N:
+    #         self.reset_increment()
+    #     # Decrement experiment trial
+    #     elif key == glfw.KEY_P:
+    #         print("Pressed P")
+    #         self.seed = max(self.seed - 1, 0)
+    #         self.env.seed(self.seed)
+    #         self.ob = self.env.reset()
+    #         for policy in self.policies:
+    #             policy.reset()
+    #         if hasattr(self.env, "reset_goal"):
+    #             self.goal = self.env.reset_goal()
+    #         self.update_sim(self.env.unwrapped.sim)
+
+    # def run(self):
+    #     if self.duration is not None:
+    #         self.end_time = time.time() + self.duration
+    #     self.total_rew_avg = 0.0
+    #     self.n_episodes = 0
+    #     while self.duration is None or time.time() < self.end_time:
+
+    #         if len(self.policies) == 1:
+    #             action, _ = self.policies[0].act(self.ob)
+    #         else:
+    #             self.ob = splitobs(self.ob, keepdims=False)
+    #             ob_policy_idx = np.split(np.arange(len(self.ob)), len(self.policies))
+    #             actions = []
+    #             for i, policy in enumerate(self.policies):
+    #                 inp = itemgetter(*ob_policy_idx[i])(self.ob)
+    #                 inp = listdict2dictnp([inp] if ob_policy_idx[i].shape[0] == 1 else inp)
+    #                 ac = policy.act(inp)
+    #                 print('action found', ac)
+    #                 actions.append(ac)
+    #             action = listdict2dictnp(actions, keepdims=True)
+
+    #         self.ob, rew, done, env_info = self.env.step(action)
+    #         self.total_rew += rew
+
+    #         if done or env_info.get('discard_episode', False):
+    #             self.reset_increment()
+
+    #         if self.display_window:
+    #             self.add_overlay(const.GRID_TOPRIGHT, "Reset env; (current seed: {})".format(self.seed), "N - next / P - previous ")
+    #             self.add_overlay(const.GRID_TOPRIGHT, "Reward", str(self.total_rew))
+    #             if hasattr(self.env.unwrapped, "viewer_stats"):
+    #                 for k, v in self.env.unwrapped.viewer_stats.items():
+    #                     self.add_overlay(const.GRID_TOPRIGHT, k, str(v))
+
+    #             self.env.render()
+
+    # def reset_increment(self):
+    #     self.total_rew_avg = (self.n_episodes * self.total_rew_avg + self.total_rew) / (self.n_episodes + 1)
+    #     self.n_episodes += 1
+    #     print(f"Reward: {self.total_rew} (rolling average: {self.total_rew_avg})")
+    #     self.total_rew = 0.0
+    #     self.seed += 1
+    #     self.env.seed(self.seed)
+    #     self.ob = self.env.reset()
+    #     for policy in self.policies:
+    #         policy.reset()
+    #     if hasattr(self.env, "reset_goal"):
+    #         self.goal = self.env.reset_goal()
+    #     self.update_sim(self.env.unwrapped.sim)
